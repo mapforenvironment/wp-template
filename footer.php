29179636ec1e34408fec853b8fdab2e2b6ac1fd5
Kristofor Carle <kristoforcarle@moabi.org>
2016-07-08 15:00:38 -0400

    add GA tracking ID

--- footer.php
+++ footer.php
@@ -45,7 +45,7 @@
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-  ga('create', '', 'auto');
+  ga('create', 'UA-80486445-1', 'auto');
   ga('send', 'pageview');
 
 </script>
