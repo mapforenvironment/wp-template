f69efa72ec94cea28937825756bcca7c38b45e8b
Kristofor Carle <kristoforcarle@moabi.org>
2016-08-24 16:36:59 -0400

    update about page

--- about.php
+++ about.php
@@ -2,109 +2,115 @@
 
 <?php get_header(); ?>
 
-<div class="headerpg" >
-  <div class="row" >
-  </div>
+<div class="headerpg">
+
 </div>
 
-<div class="content" id="intro">
+<div class="content" style="padding-bottom: 0px;">
   <div class="row text-center wp2">
     <h1>Welcome to Map for Environment!</h1>
-    <p>From space, the earth’s great forests and rivers still seem intact and flowing. The Congo Basin
-rainforest straddles Africa extending across the continent, regulating the regional climate, providing
-food and livelihoods, and home to thousands of species such as elephants and chimpanzees. Asia’s
-great rivers – the Mekong, Brahmaputra, the Yangtze, etc – meander from the head waters of the
-Himalayas before flowing down to the mouths of Vietnam Delta and the Bangladesh, providing water,
-food, and energy to hundreds of millions of people.</p>
-<p>Zoom in further, the threats to these great natural phenomena become apparent. Forests and rivers
-face both seen and unseen dangers. Forests are strafed by logging roads, which cut deep in intact
-forest areas. The loggers extract valuable timber, feeding ever growing demand for tropical
-hardwoods. They provide jobs for a few but when poorly managed, leave the forests vulnerable to
-hunting and conversion to agriculture. Hot on their heals, comes industrial agriculture, converting the
-degraded land into oil palm, cacoa, and eucalyptus plantations. Asia rivers are increasingly being
-dammed to provide energy for rapidly growing cities as well as diverting water to arid regions.
-Many of these activities are visible from satellites but unmapped, and their scale and impacts
-undocumented. This lack of transparency creates a culture of impunity that allows bad actors to go
-unpunished and good actors to go unrecognized. Transparency creates a level playing field.</p>
+    <h3>Use OpenStreetMap to help protect the world’s forests, rivers, and plains</h3>
+  </div>
+</div>
+<div class="content" style="padding-top: 0px; padding-bottom: 30px;">
+  <div class="row wp2">
+    <iframe src="https://maphubs.com/map/embed/212/static" style="width: 1024px; height: 550px; max-width: 100%;" frameborder="0"></iframe>
+
+    <p>
+      From space, the earth’s great forests and rivers still seem intact and flowing. The Congo Basin rainforest straddles Africa extending across the continent, regulating the regional climate, providing food and livelihoods to millions, and refuge to thousands of species including forest elephants and chimpanzees.
+    </p>
+    <iframe src="https://maphubs.com/map/embed/36/static" style="width: 1024px; height: 550px; max-width: 100%;" frameborder="0"></iframe>
+
+    <p>
+      Asia’s great rivers – the Mekong, Brahmaputra, the Yangtze, etc – meander from the head waters of the Himalayas before flowing down to the mouths of Vietnam Delta and the Bangladesh, providing water, food, and energy to hundreds of millions of people. And the vast plains of the Dakotas still remain some of most vast unpopulated regions of the United States.
+    </p>
+    <div style="width: 100%; text-align: center; padding: 25px 0px;">
+      <img src="<?php bloginfo('template_directory'); ?>/img/about/Fracking.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
+      By <a href="//commons.wikimedia.org/w/index.php?title=User:Joshua_Doubek&amp;action=edit&amp;redlink=1" class="new" title="User:Joshua Doubek (page does not exist)">Joshua Doubek</a> - <span class="int-own-work" lang="en">Own work</span>, <a href="http://creativecommons.org/licenses/by-sa/3.0" title="Creative Commons Attribution-Share Alike 3.0">CC BY-SA 3.0</a>, <a href="https://commons.wikimedia.org/w/index.php?curid=27524446">https://commons.wikimedia.org/w/index.php?curid=27524446</a>
+    </div>
+    <p>
+      Zoom in further, the threats to these great natural phenomena become apparent. Forests and rivers face both seen and unseen dangers. Forests are strafed by logging roads, which cut deep in intact forest areas. The loggers extract valuable timber, feeding ever growing demand for tropical hardwoods. They provide jobs for a few but when poorly managed, leave the forests vulnerable to hunting and conversion to agriculture.
+    </p>
+    <div style="width: 100%; text-align: center; padding: 25px 0px;">
+      <img src="<?php bloginfo('template_directory'); ?>/img/about/TessoNiloNationalPark.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
+    </div>
+    <p>
+      Hot on their heels, comes industrial agriculture, converting the degraded land into oil palm, cacao, and eucalyptus plantations. Asia rivers are increasingly being dammed to provide energy for rapidly growing cities as well as diverting water to arid regions. And fracking sites dot the plains across the North America. Many of these activities are visible from satellites but unmapped, and their scale and impacts undocumented. This lack of transparency creates a culture of impunity that allows bad actors to go unpunished and good actors to go unrecognized.
+    </p>
   </div>
 </div>
 
-<div  class="content" style="background: url('<?php bloginfo('template_directory'); ?>/img/deforestation.jpg');height: 100%;background-size:cover; background-position: center;"><img src=""></div>
-
-<div class="grey content" style="padding: 0px">
-  <div class="wp2">
-
-    <div class="large-12 columns" style="padding: 75px 60px">
-      <h2>The goal</h2>
-      <p></p>
-      <p>The goal of Map for Environment is to make the invisible visible. We are quite literally aiming to put
-logging roads, industrial agriculture, and dams on the map. The availability of high-resolution satellite
-information, open data portals such as MapHubs and Global Forest Watch has placed an
-unprecedented amount of environmental data in to public domain. OpenStreetMap – the crowdsourced
-map of the world – has also built a global community of volunteers mapping the planet. The OSM
-community has also pioneered crowd sourced mapping to support humanitarian organizations
-responding to disasters such as the Nepal earthquake and the tsunami in the Philippines. Map for
-Environment follows this model to map the unmapped but we focus on the humanitarian disaster
-slowly unraveling in the world’s critical ecosystems.</p>
+<div class="content" style="padding-top: 0px; padding-bottom: 30px;">
+  <div class="row wp2">
+    <h2>The goal</h2>
+    <p>Map for Environment’s aim is to quite literally put logging roads, industrial agriculture, dams, and fracking on the map. The availability of high-resolution satellite information, open data portals such as Global Forest Watch (<a href="http://globalforestwatch.org" target="_blank">globalforestwatch.org</a>) and MapHubs (<a href="https://maphubs.com" target="_blank">maphubs.com</a>) has placed an unprecedented amount of environmental data in to public domain. OpenStreetMap (<a href="http://openstreetmap.org" target="_blank">openstreetmap.org</a>) – the crowdsourced map of the world – has also built a global community of volunteers mapping the planet.</p>
+    <div style="width: 100%; text-align: center; padding: 25px 0px;">
+      <img src="<?php bloginfo('template_directory'); ?>/img/about/sotmus2016.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
     </div>
-    </div>
-</div>
-<div class="content" style="background: url('<?php bloginfo('template_directory'); ?>/img/mapping_palmmill.png');height: 100%;background-size:cover; background-position: center;"><img src=""></div>
-<div class="content" style="padding: 0px">
-  <div class="wp2">
-    <div class="large-12 columns" style="padding: 60px">
-      <h2>How it works</h2>
-      <p></p>
-      <p>Map for Environment uses OpenStreetMap tools - combined with satellite imagery and location data of
-known logging, industrial agriculture, and dam locations – to create detailed maps of infrastructure, size, as
-well as communities impacted. The information is mapped by volunteers who we teach how to use
-OpenStreetMap tools. Mapped data is shared on OpenStreetMap and also
-available on MapHubs – an open data repository where users can combine with other data to make
-and publish interactive maps. The Map for Environment team uses both open data and expert advice
-to direct efforts to dams, logging roads, and industrial agriculture plantations operating in high risk
-areas. This is ensures that we make maps that have impact that will directly support advocates and
-agencies on the ground hold operators to account for illegal and unsustainable practices.</p>
-    </div>
+    <p>The OSM community has pioneered crowd sourced mapping to support humanitarian organizations responding to disasters such as the Nepal earthquake and the 2013 Typhoon Yolanda/Haiyan in the Philippines. Map for Environment follows this model to map the unmapped environmmental disaster slowly unraveling in the world’s critical ecosystems.</p>
   </div>
 </div>
 
-<div class="content" id="intro">
-  <div class="row text-center wp2">
-    <h1>Three initiatives</h1>
+<div class="content" style="padding-top: 0px; padding-bottom: 30px;">
+  <div class="row wp2">
+    <h2>How it works</h2>
+    <p>Map for Environment combines OpenStreetMap mapping tools with satellite imagery and location data of known logging, industrial agriculture, dam, and fracking locations to create detailed maps of infrastructure, size, as well as communities impacted. The information is mapped by OpenStreetMap users. Mapped data is shared on OpenStreetMap and will also available on MapHubs – a simple-to-use interactive map publishing platform.</p>
+    <div style="width: 100%; text-align: center; padding: 25px 0px;">
+      <img src="<?php bloginfo('template_directory'); ?>/img/about/MappingPartyKinshasa.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
+    </div>
+    <p>The Map for Environment team uses both open data and expert advice to direct efforts to maps dams, logging roads, industrial agriculture plantations, and fracking sites operating in high risk areas. This is ensures that we make high impact maps that directly support advocates and agencies holding operators accountable for illegal, unsustainable, and dangerous practices.</p>
   </div>
 </div>
-<div class="work">
-  <figure class="large-4 columns">
-    <img src="<?php bloginfo('template_directory'); ?>/img/loggingroads.png" >
 
-    <figcaption>
-      <h2>Logging Roads</h2>
-      <p>In partnership with Global Forest Watch, The award winning Logging Roads initiative
-      (<a href="http://loggingroads.org">loggingroads.org</a>) aims to build a complete and dynamic map of all the world’s logging roads,
-      particularly focused in the tropics. With the support of volunteers, Logging Roads has already mapped
-      more than 30,000 roads with over 12,000 user edits in the Congo Basin. This data supports forest
-      monitors identify violations by logging companies, encroachment into indigenous people’s territories,
-      and risks to wildlife conservation.</a></p>
-    </figcaption>
-  </figure>
-  <figure class="large-4 columns">
-    <img src="<?php bloginfo('template_directory'); ?>/img/industrialagriculture.png" >
-    <figcaption>
-      <h2>Industrial Agriculture</h2>
-      <p>Map for Environment aims to map all industrial agriculture operating in or near the tropics with a
-        specific focus on oil palm in Indonesia and the Congo Basin. Oil palm plantations, in particular,have
-        decimated rainforest in Sumatra and Borneo, creating conflicts with indigenous communities and
-        clearing habitat for the endanger orangutan and tiger populations. But despite their enormous impact,
-        very few plantations and their associated infrastructure such as mills have been mapped.</p>
-    </figcaption>
-  </figure>
-  <figure class="large-4 columns">
-    <img src="<?php bloginfo('template_directory'); ?>/img/asiadams.png" >
-    <figcaption>
-      <h2>Hydropower in Asia</h2>
-      <p>Mapping hydropower dam sites and at risk areas upstream helps us understand the potential impact of new dams.</p>
-    </figcaption>
-  </figure>
+<div class="content" style="padding-top: 0px; padding-bottom: 30px;">
+  <div class="row wp2">
+    <h2>Join Us</h2>
+    <h4>There are lots of ways you can get involved with Map for Environment.</h4>
+    <h3 style="padding: 0px;">Help map!</h3>
+    <p>Anyone can help map by creating an OpenStreetMap account, learn the basics, and taking on a mapping task. It takes no time at all to begin actively mapping and making an important contribution to environmental conservation efforts.</p>
+    <h3 style="padding: 0px;">Host a mapping party!</h3>
+    <p>Do you have some space to accommodate some eager mappers and an internet connection? They you could be a mapping party host! Mapping parties are a great way to accomplish a mapping task and meet fellow mappers. Providing some beers and pizza goes a long way too. Please contact us, if you are interested in hosting a party.</p>
+    <h3 style="padding: 0px;">Become a partner!</h3>
+    <p>Map for Environment is completely run by volunteers. If you would like to provide financial or technical support to the project, please contact us here. Funding helps us improve the tools, sponsor mapping parties in places like DRC and Indonesia and helps us obtain proprietary high resolution imagery, which is critical to mapping. If you would like to sponsor Map for Environment, please contact us.</p>
+  </div>
+</div>
+
+<div class="content" style="padding-top: 0px; padding-bottom: 0px;">
+  <div class="row wp2">
+    <h2>Initiatives</h2>
+  </div>
+</div>
+<div class="content" style="padding-top: 0px; padding-bottom: 0px;">
+  <div class="row">
+    <figure class="large-3 columns">
+      <img src="<?php bloginfo('template_directory'); ?>/img/loggingroads.png" >
+      <figcaption>
+        <h2 style="font-size 1.4em !important;">Logging Roads</h2>
+        <p>Help build a complete map of logging roads in the world's remaining rainforests</p>
+      </figcaption>
+    </figure>
+    <figure class="large-3 columns">
+      <img src="<?php bloginfo('template_directory'); ?>/img/industrialagriculture.png" >
+      <figcaption>
+        <h2 style="font-size 1.4em !important;">Industrial Agriculture</h2>
+        <p>Map the impact of industrial agriculture on Congo Basin and Indonesian rainforests</p>
+      </figcaption>
+    </figure>
+    <figure class="large-3 columns">
+      <img src="<?php bloginfo('template_directory'); ?>/img/asiadams.png" >
+      <figcaption>
+        <h2 style="font-size 1.4em !important;">Hydropower in Asia</h2>
+        <p>Mapping hydropower existing and planned dams on Asia's great rivers</p>
+      </figcaption>
+    </figure>
+    <figure class="large-3 columns">
+      <img src="<?php bloginfo('template_directory'); ?>/img/fracking.png" >
+      <figcaption>
+        <h2 style="font-size 1.4em !important;">Fracking</h2>
+        <p>Mapping the boom of hydraulic fracturing gas exploration sites in the U.S.</p>
+      </figcaption>
+    </figure>
+  </div>
 </div>
 
 <?php get_footer(); ?>
