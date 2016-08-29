422d0bb0ce272ceca5ce4b6ebc501f5aadc6ba4a
Kristofor Carle <kristoforcarle@moabi.org>
2016-08-29 00:48:31 -0400

    add contribute sections to initiative pages

--- fracking.php
+++ fracking.php
@@ -25,7 +25,29 @@
     <p>We plan to put them all on the map. <a href="https://tasks.mapforenvironment.org/">Click here</a> to help us!</p>
   </div>
 </div>
-
+<div class="dark" >
+  <div class="row wp1">
+    <h1>Contribute Now</h1>
+    <p class="headline">Use our OpenStreetMap tasking manager to choose a task and start mapping!</p>
+    <div class="large-3 columns">
+      <a href="https://loggingroads.org"><button class="animated fadeInDown">Map Logging Roads</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Agriculture</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Hydropower</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Fracking</button></a>
+    </div>
+    <p>See our <a href="/get-started">Get Started</a> page for more information</p>
+  </div>
+  <div class="row wp1" style="padding-top: 15px;">
+    <h1>How can I help?</h1>
+    <p>Host a mapping party, tell us about other areas to map, or become a partner. Questions? <a class="beacon-open">Send us a message.</a></p>
+  </div>
+</div>
 
 
 <?php get_footer(); ?>
--- hydropower.php
+++ hydropower.php
@@ -25,7 +25,7 @@
     <p>1) Map existing and planned dam locations – we have a database of several hundred dams in Asia in MapHubs. Using iD editor, users can edit the location of dams and other vital information sich as the name of the dam, the operator, financier, and other relevant pieces of data.</p>
     <p>2) Mapping communities impacted – For a number of high priority dams, we have set up tasks to map villages and roads that will likely be affected. Most of these communities have not been mapped in either OpenStreetMap or Google.</p>
     <div style="width: 100%; text-align: center; padding: 25px 0px;">
-      
+
       <img src="<?php bloginfo('template_directory'); ?>/img/about/Don_Sahong_Dam_Site_part1.jpg" style="max-width: 50%; height: auto; max-height: 500px; margin: auto; text-align:center;" >
       <img src="<?php bloginfo('template_directory'); ?>/img/about/Don_Sahong_Dam_Site_part2.jpg" style="max-width: 50%; height: auto; max-height: 500px; margin: auto; text-align:center;" >
       <p style="font-size: 14px !important;">Example of a village near the dam site that is not yet mapped in OpenStreetMap</p>
@@ -33,6 +33,29 @@
     <h3 style="padding-bottom: 0px;">BENEFITS</h3>
     <p>A dynamic map of dam locations, their names, status, and companies responsible will be an invaluable resource for researchers, environmental protection agencies, and advocacy organizations. It will help hold dam builders accountable, support studies impact of dams on entire basins and sub basins, and provide groups on the ground with better maps to advocate for rights of those affected. And because we are using OpenStreetMap, the data is completely open, so anyone to download and use the data.</p>
   </div>
-</div
+</div>
+<div class="dark" >
+  <div class="row wp1">
+    <h1>Contribute Now</h1>
+    <p class="headline">Use our OpenStreetMap tasking manager to choose a task and start mapping!</p>
+    <div class="large-3 columns">
+      <a href="https://loggingroads.org"><button class="animated fadeInDown">Map Logging Roads</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Agriculture</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Hydropower</button></a>
+    </div>
+    <div class="large-3 columns">
+      <a href="https://tasks.mapforenvironment.org"><button class="animated fadeInDown">Map Fracking</button></a>
+    </div>
+    <p>See our <a href="/get-started">Get Started</a> page for more information</p>
+  </div>
+  <div class="row wp1" style="padding-top: 15px;">
+    <h1>How can I help?</h1>
+    <p>Host a mapping party, tell us about other areas to map, or become a partner. Questions? <a class="beacon-open">Send us a message.</a></p>
+  </div>
+</div>
 
 <?php get_footer(); ?>
