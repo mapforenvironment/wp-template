2f489473db12d02a6d34d0f12e7b57d1c3752830
Kristofor Carle <kristoforcarle@moabi.org>
2016-08-29 00:49:07 -0400

    about page updates

--- about.php
+++ about.php
@@ -22,7 +22,7 @@
     <iframe class="maphubs-map" src="https://maphubs.com/map/embed/36/static" frameborder="0"></iframe>
 
     <p>
-      Asia’s great rivers – the Mekong, Brahmaputra, the Yangtze, etc – meander from the head waters of the Himalayas before flowing down to the mouths of Vietnam Delta and the Bangladesh, providing water, food, and energy to hundreds of millions of people. And the vast plains of the Dakotas still remain some of most vast unpopulated regions of the United States.
+      Asia’s great rivers – the Mekong, Brahmaputra, the Yangtze, etc – meander from the head waters of the Himalayas before flowing down to the mouths of Vietnam Delta and the Bangladesh, providing water, food, and energy to hundreds of millions of people. And the vast plains of the Dakotas still remain some of most sparsely populated regions of the United States.
     </p>
     <div style="width: 100%; text-align: center; padding: 25px 0px;">
       <img src="<?php bloginfo('template_directory'); ?>/img/about/Fracking.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
@@ -35,7 +35,7 @@
       <img src="<?php bloginfo('template_directory'); ?>/img/about/TessoNiloNationalPark.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
     </div>
     <p>
-      Hot on their heels, comes industrial agriculture, converting the degraded land into oil palm, cacao, and eucalyptus plantations. Asia rivers are increasingly being dammed to provide energy for rapidly growing cities as well as diverting water to arid regions. And fracking sites dot the plains across the North America. Many of these activities are visible from satellites but unmapped, and their scale and impacts undocumented. This lack of transparency creates a culture of impunity that allows bad actors to go unpunished and good actors to go unrecognized.
+      Hot on their heels, comes industrial agriculture, converting the degraded land into oil palm, cacao, and eucalyptus plantations. Asia rivers are increasingly being dammed to provide energy for rapidly growing cities as well as diverting water to arid regions. And fracking sites dot the plains across North America. Many of these activities are visible from satellites but unmapped, and their scale and impacts undocumented. This lack of transparency creates a culture of impunity that allows bad actors to go unpunished and good actors to go unrecognized.
     </p>
   </div>
 </div>
@@ -46,6 +46,7 @@
     <p>Map for Environment’s aim is to quite literally put logging roads, industrial agriculture, dams, and fracking on the map. The availability of high-resolution satellite information, open data portals such as Global Forest Watch (<a href="http://globalforestwatch.org" target="_blank">globalforestwatch.org</a>) and MapHubs (<a href="https://maphubs.com" target="_blank">maphubs.com</a>) has placed an unprecedented amount of environmental data in to public domain. OpenStreetMap (<a href="http://openstreetmap.org" target="_blank">openstreetmap.org</a>) – the crowdsourced map of the world – has also built a global community of volunteers mapping the planet.</p>
     <div style="width: 100%; text-align: center; padding: 25px 0px;">
       <img src="<?php bloginfo('template_directory'); ?>/img/about/sotmus2016.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
+      State of the Map US 2016 in Seattle
     </div>
     <p>The OSM community has pioneered crowd sourced mapping to support humanitarian organizations responding to disasters such as the Nepal earthquake and the 2013 Typhoon Yolanda/Haiyan in the Philippines. Map for Environment follows this model to map the unmapped environmmental disaster slowly unraveling in the world’s critical ecosystems.</p>
   </div>
@@ -54,9 +55,10 @@
 <div class="content" style="padding-top: 0px; padding-bottom: 30px;">
   <div class="row wp2">
     <h2>How it works</h2>
-    <p>Map for Environment combines OpenStreetMap mapping tools with satellite imagery and location data of known logging, industrial agriculture, dam, and fracking locations to create detailed maps of infrastructure, size, as well as communities impacted. The information is mapped by OpenStreetMap users. Mapped data is shared on OpenStreetMap and will also available on MapHubs – a simple-to-use interactive map publishing platform.</p>
+    <p>Map for Environment combines OpenStreetMap mapping tools with satellite imagery and location data of known logging, industrial agriculture, dam, and fracking locations to create detailed maps of infrastructure, size, as well as communities impacted. The information is mapped by OpenStreetMap users. Mapped data is shared on OpenStreetMap and will also available on <a href="https://maphubs.com" target="_blank">MapHubs</a> – a simple-to-use interactive map publishing platform.</p>
     <div style="width: 100%; text-align: center; padding: 25px 0px;">
       <img src="<?php bloginfo('template_directory'); ?>/img/about/MappingPartyKinshasa.jpg" style="max-width: 100%; height: auto; margin: auto; text-align:center;" >
+      a <a href="http://loggingroads.org" target="_blank">Logging Roads</a> mapping party in Kinshasa, DRC
     </div>
     <p>The Map for Environment team uses both open data and expert advice to direct efforts to maps dams, logging roads, industrial agriculture plantations, and fracking sites operating in high risk areas. This is ensures that we make high impact maps that directly support advocates and agencies holding operators accountable for illegal, unsustainable, and dangerous practices.</p>
   </div>
